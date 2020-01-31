055fd09bdaa29cc38b81f50a9d809205ec2860bd
Marcus Bauer <marcus@bauer.wf>
2020-01-31 23:41:14 +0100

    unit test project (empty)

---
+++ Pineapple.UnitTests/Demo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Pineapple.UnitTests
+{
+    public sealed class Demo
+    {
+        [Fact]
+        public void SomewhatPassingTest()
+        {
+            Assert.Equal(3, 3);
+        }
+    }
+}
