15899d9bcb344be0cf3844ae38d3dc42f4467a82
Marcus Bauer <marcus@bauer.wf>
2020-02-01 01:33:20 +0100

    display InvalidSpaceNameException with actual name

--- Pineapple.Domain/Spaces/ValueObjects/SpaceName.cs
+++ Pineapple.Domain/Spaces/ValueObjects/SpaceName.cs
@@ -9,7 +9,7 @@ namespace Pineapple.Domain.Spaces.ValueObjects
     public struct SpaceName
     {
         private static readonly Regex ValidSpaceName = new Regex(@"^([a-zA-Z\-_]{1,32})$", RegexOptions.Compiled);
-        
+
         private readonly string _name;
 
         /// <summary>
@@ -20,9 +20,9 @@ namespace Pineapple.Domain.Spaces.ValueObjects
         {
             if (string.IsNullOrEmpty(name))
                 throw new InvalidSpaceNameException("No name was provided for the space.");
-            
+
             if (!ValidSpaceName.IsMatch(name))
-                throw new InvalidSpaceNameException("The space name '' is invalid.");
+                throw new InvalidSpaceNameException($"The space name '{name}' is invalid.");
 
             _name = name;
         }
