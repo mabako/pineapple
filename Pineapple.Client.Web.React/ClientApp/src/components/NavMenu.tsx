724047155f8d0d42b19e23f7a2d4640bb07dc02d
Marcus Bauer <marcus@bauer.wf>
2020-02-08 00:05:24 +0100

    link to api doc in the navigation menu

--- Pineapple.Client.Web.React/ClientApp/src/components/NavMenu.tsx
+++ Pineapple.Client.Web.React/ClientApp/src/components/NavMenu.tsx
@@ -37,6 +37,9 @@ export class NavMenu extends Component<{}, ILocalState> {
                 <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
                 </NavItem>
+                <NavItem>
+                  <a href="/$/swagger/" className="text-dark nav-link">API</a>
+                </NavItem>
               </ul>
             </Collapse>
           </Container>
