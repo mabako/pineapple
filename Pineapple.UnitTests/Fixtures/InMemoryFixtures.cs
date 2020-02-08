b92b4f4a42b45d483d285eba1cf2c1e5cc570154
Marcus Bauer <marcus@bauer.wf>
2020-02-08 13:30:54 +0100

    add fixtures prototype for in memory

---
+++ Pineapple.UnitTests/Fixtures/InMemoryFixtures.cs
@@ -0,0 +1,33 @@
+﻿#if false
+using Pineapple.Domain.Pages;
+using Pineapple.Domain.Spaces;
+using Pineapple.Infrastructure.DataAccess.InMemory;
+using Pineapple.Infrastructure.DataAccess.InMemory.Repositories;
+
+namespace Pineapple.UnitTests.Fixtures
+{
+    public sealed class InMemoryFixtures
+    {
+        public InMemoryFixtures()
+        {
+            Context = new InMemoryContext();
+
+            var entityFactory = new InMemoryEntityFactory();
+            SpaceFactory = entityFactory;
+            PageFactory = entityFactory;
+
+            SpaceRepository = new SpaceRepository(Context);
+            PageRepository = new PageRepository(Context);
+
+            SpaceService = new SpaceService(SpaceFactory, SpaceRepository);
+        }
+
+        public InMemoryContext Context { get; }
+        public ISpaceFactory SpaceFactory { get; }
+        public IPageFactory PageFactory { get; }
+        public ISpaceRepository SpaceRepository { get; }
+        public IPageRepository PageRepository { get; }
+        public SpaceService SpaceService { get; }
+    }
+}
+#endif
